27fb70fdfa0ed4d3278f0a70396e4348e7eb7d98
Fagner202 <109249175+Fagner202@users.noreply.github.com>
2024-08-18 10:29:48 -0300

    #aula09 Templates no Blade

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,10 +1,15 @@
-<h1>Novo Usuário</h1>
+@extends('admin.layouts.app')
 
-<form action="{{ route('users.store') }}" method="POST">
-    {{-- <input type="text" name="_token" value="{{ csrf_token() }}"> --}}
-    @csrf()
-    <input type="text" name="name" placeholder="Nome">
-    <input type="text" name="email" placeholder="Email">
-    <input type="text" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+@section('title', 'Cria usuário')
+@section('content')
+    <h1>Novo Usuário</h1>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        {{-- <input type="text" name="_token" value="{{ csrf_token() }}"> --}}
+        @csrf()
+        <input type="text" name="name" placeholder="Nome">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="password" placeholder="Senha">
+        <button type="submit">Enviar</button>
+    </form>
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('admin.layouts.app')
+
+@section('title', 'Lista usuários')
+@section('content')
+
     <h1>Usuários</h1>
 
     <a href="{{ route('users.create') }}">Novo</a>
@@ -32,5 +29,5 @@
     </table>
 
     {{ $users->links() }}
-</body>
-</html>
+
+@endsection
