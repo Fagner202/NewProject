860ad9994500b71f26bd6d49cb27e54f19615732
Fagner202 <109249175+Fagner202@users.noreply.github.com>
2024-08-18 00:45:04 -0300

    #aula06 MVC (models, views e controllers)

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    
+    public function index()
+    {
+        $user = User::first();
+
+        return view('admin.users.index', [
+            'user' => $user
+        ]);
+        // return 'UserController@index';
+    }
+}
