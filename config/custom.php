a169f58748fffcb0c1b7d743b75a2570b04b6a54
Fagner202 <109249175+Fagner202@users.noreply.github.com>
2024-08-26 21:45:41 -0300

    #aula17 Middlewares no Laravel 11 (filtros)

---
+++ config/custom.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'admins' => [
+        'fagnersilveira6@gmail.com',
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,22 @@
 
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckIfsAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIfsAdmin::class);    
+});
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
