5c685a9c8936a3f2ac96aadfd067b090b903b8d4
Fagner202 <109249175+Fagner202@users.noreply.github.com>
2024-09-07 21:08:07 -0300

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -61,20 +61,19 @@
     <body>
         <div class="navbar navbar-expand-lg navbar-primary bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">eventos</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#">Eventos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sobre</a>
+                            <a class="nav-link" href="#">Usúario</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contato</a>
+                            <a class="nav-link" href="#">Admin</a>
                         </li>
                     </ul>
                     <!-- Container para o ícone, alinhado à direita -->
