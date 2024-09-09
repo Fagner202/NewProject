ced090098ca2f1ac4cc98739feaecfee86bb6b01
Fagner202 <109249175+Fagner202@users.noreply.github.com>
2024-09-09 19:56:32 -0300

    #3: aplicando swal alert

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -50,27 +50,52 @@
 
 <script>
     function myFunction(eventId) {
-        if (confirm('Tem certeza de que deseja excluir este evento?')) {
-            fetch(`/eventos/${eventId}`, {
-                method: 'DELETE',
-                headers: {
-                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    alert('Evento excluído com sucesso!');
-                    location.reload(); // Recarrega a página para refletir a exclusão
-                } else {
-                    alert('Ocorreu um erro ao tentar excluir o evento.');
-                }
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Ocorreu um erro ao tentar excluir o evento.');
-            });
-        }
+            Swal.fire({
+            title: 'Tem certeza?',
+            text: "Você não poderá reverter isso!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, excluir!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`/eventos/${eventId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        Swal.fire(
+                            'Excluído!',
+                            'O evento foi excluído com sucesso.',
+                            'success'
+                        ).then(() => {
+                            location.reload(); // Recarrega a página para refletir a exclusão
+                        });
+                    } else {
+                        Swal.fire(
+                            'Erro!',
+                            'Ocorreu um erro ao tentar excluir o evento.',
+                            'error'
+                        );
+                    }
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    Swal.fire(
+                        'Erro!',
+                        'Ocorreu um erro ao tentar excluir o evento.',
+                        'error'
+                    );
+                });
+            }
+        });
+
     }
 </script>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -93,5 +93,11 @@
 
     <!-- Custom JS (sem Vite) -->
     <script src="{{ asset('js/theme.js') }}"></script>
+
+    <!-- Swal Alert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+    </script>
 </body>
 </html>
